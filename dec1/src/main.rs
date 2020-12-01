a8bab01594dbe96ea4a4aad755947125b275f001
Rob N <walrusk@gmail.com>
2020-12-01 17:47:15 -0600

    dec 1 part 2

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -3,28 +3,42 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-  let f = File::open("./input.txt").expect("Unable to open file");
-  let f = BufReader::new(f);
-  let mut winning_numbers = HashMap::new();
+  let mut winning_numbers: HashMap<i32, Vec<&i32>> = HashMap::new();
+  let numbers = read_input("./input.txt");
 
-  for line in f.lines() {
-    let n = line.expect("Unable to read line");
-    let n = n.parse::<i32>().unwrap();
-    let n_partner = 2020 - n;
-    let found = winning_numbers.get(&n);
+  // pre-sum pairs
+  for n in numbers.iter() {
+    for m in numbers.iter() {
+      let n_dbl_partner = n + m;
+      winning_numbers.insert(n_dbl_partner, vec![n, m]);
+    }
+  }
+
+  // find third
+  for n in numbers.iter() {
+    let found = winning_numbers.get(&(2020 - n));
     if !found.is_none() {
-      found_win(n, n_partner);
+      found_win(&n, found.unwrap()[0], found.unwrap()[1]);
       break;
     }
-    winning_numbers.insert(n_partner, 1);
   }
 }
 
-fn found_win(num1: i32, num2: i32) {
+fn read_input(filename: &str) -> Vec<i32> {
+  let f = File::open(filename).expect("Unable to open file");
+  let f = BufReader::new(f);
+  return f
+    .lines()
+    .map(|l| l.unwrap().parse::<i32>().unwrap())
+    .collect();
+}
+
+fn found_win(num1: &i32, num2: &i32, num3: &i32) {
   println!(
-    "we found a winning number! {}*{} = {}",
+    "we found some winning numbers! {}*{}*{} = {}",
     num1,
     num2,
-    num1 * num2
+    num3,
+    num1 * num2 * num3
   );
 }
